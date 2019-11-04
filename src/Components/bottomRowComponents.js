7e7829b9478a0be108ce7b4aef652efcf99c0316
Maxwell Beard <maxwelljamesbeard@gmail.com>
2019-11-04 14:35:52 +0000

    components created

---
+++ src/Components/bottomRowComponents.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const bottomRowComponents = (props) => {
+    const {title, value} = props;
+    return (
+        <div className="down">
+            <h3 className="down__title">{title}</h3>
+            <div className="down_value">{value}</div>
+        </div>
+    )
+};
+
+export default bottomRowComponents;
