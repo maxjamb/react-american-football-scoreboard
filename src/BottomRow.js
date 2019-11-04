9bf5e6029df33e3db50b54b4d3aa890f6aba5957
Maxwell Beard <maxwelljamesbeard@gmail.com>
2019-11-04 14:59:49 +0000

    components created, css styling bug

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import BottomRowComponents from './Components/bottomRowComponents';
+import BottomRowComponents from './Components/BottomRowComponents';
 const BottomRow = () => {
   return (
     <div className="bottomRow">
